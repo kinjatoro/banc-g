03ec365b79b2ed00aa388185030a6e8d1609a8b4
kinjatoro <lucaslopezvazquez1@gmail.com>
2023-09-12 17:00:55 -0300

    icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
 import { Button } from '@mui/material';
+import React from 'react';
+
 
 function App() {
   return (
     <div>
-      <Button>
-        PLACEHOLDER
+      <Button 
+      color="primary"
+      variant="contained"
+      disableElevation="true"
+      href="https://youtu.be/hHRz5USvrXY"
+      size="large"
+      >
+
+        HOLA
       </Button>
     </div>
   );
