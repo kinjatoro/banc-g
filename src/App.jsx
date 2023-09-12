874d3074ed00856d2957e415da1a4e441217b1fd
kinjatoro <lucaslopezvazquez1@gmail.com>
2023-09-12 14:35:08 -0300

    restart

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <Button>
+        PLACEHOLDER
+      </Button>
+    </div>
+  );
+}
+
+export default App;
