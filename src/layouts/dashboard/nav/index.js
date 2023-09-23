7f5dddd183b2ebd0ee2f523613b40b65ab3b7c5a
kinjatoro <lucaslopezvazquez1@gmail.com>
2023-09-23 17:16:31 -0300

    actualizado logo

--- src/layouts/dashboard/nav/index.js
+++ src/layouts/dashboard/nav/index.js
@@ -73,8 +73,11 @@ export default function Nav({ openNav, onCloseNav }) {
         '& .simplebar-content': { height: 1, display: 'flex', flexDirection: 'column' },
       }}
     >
-      <Box sx={{ px: 2.5, py: 3, display: 'inline-flex' }}>
+      
+      <Box sx={{ px: 2.5, pb: 1, pt:3, display: 'inline-flex'  }}>
         <Logo />
+        PRUEBA
+        
       </Box>
 
       {auth ? (
