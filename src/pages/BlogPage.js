68d61cae31508221a48aff867b170cd794d797ff
kinjatoro <lucaslopezvazquez1@gmail.com>
2023-10-26 01:08:05 -0300

    filtrar ahora está en la misma pagina que blogpage

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -2,9 +2,23 @@ import { useState } from 'react';
 
 import { Helmet } from 'react-helmet-async';
 // @mui
-import { Grid, Button, Container, Stack, Typography, Box, Menu, MenuItem } from '@mui/material';
+import { Grid, Button, Container,  Menu, MenuItem,  Box,
+  Stack,
+
+  Drawer,
+  Rating,
+  Divider,
+  Checkbox,
+  FormGroup,
+  IconButton,
+  Typography,
+  RadioGroup,
+  FormControlLabel} from '@mui/material';
+
+
 
 import jwtDecode from 'jwt-decode';
+import Scrollbar from '../components/scrollbar';
 
 import { ProductSort} from '../sections/@dashboard/products';
 
@@ -22,6 +36,10 @@ import { useMyBar } from '../TengoBarAuth'
 
 // ----------------------------------------------------------------------
 
+export const FILTER_CATEGORIA_OPTIONS = ['Matemática', 'Música', 'Química', 'Historia', 'Geografía', 'Inglés', 'Programación' ];
+export const FILTER_TIPOCLASE_OPTIONS = ['Individual', 'Grupal'];
+export const FILTER_RATING_OPTIONS = ['up5Star', 'up4Star', 'up3Star', 'up2Star', 'up1Star'];
+export const FILTER_FRECUENCIA_OPTIONS = ['Única', 'Semanal', 'Mensual'];
 
 
 export default function BlogPage() {
@@ -172,11 +190,9 @@ export default function BlogPage() {
             />
               
             <Box sx={{textAlign: "right"}}>
-            <ProductFilterSidebar
-              openFilter={openFilter}
-              onOpenFilter={handleOpenFilter}
-              onCloseFilter={handleCloseFilter}
-            />
+            <Button disableRipple color="inherit" endIcon={<Iconify icon="ic:round-filter-list" />} onClick={handleOpenFilter}>
+                Filtrar&nbsp;
+             </Button>
 
 
 
@@ -265,6 +281,134 @@ export default function BlogPage() {
           ))}
         </Grid>
       </Container>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <Drawer
+        anchor="right"
+        open={openFilter}
+        onClose={handleCloseFilter}
+        PaperProps={{
+          sx: { width: 280, border: 'none', overflow: 'hidden' },
+        }}
+      >
+        <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ px: 1, py: 2 }}>
+        
+          <Typography variant="subtitle1" sx={{ ml: 1 }}>
+            Filtrar
+          </Typography>
+          <IconButton onClick={handleCloseFilter}>
+            <Iconify icon="eva:close-fill" />
+          </IconButton>
+        </Stack>
+
+        <Divider />
+
+        <Scrollbar>
+          <Stack spacing={3} sx={{ p: 3 }}>
+            <div> 
+             
+                
+              <Typography variant="subtitle1" gutterBottom >
+                Categoría
+              </Typography>
+             
+              <FormGroup>
+                {FILTER_CATEGORIA_OPTIONS.map((item) => (
+                  <FormControlLabel key={item} control={<Checkbox />} label={item} />
+                ))}
+              </FormGroup>
+            </div>
+
+            <div>
+              <Typography variant="subtitle1" gutterBottom>
+                Tipo de Clase
+              </Typography>
+              <RadioGroup>
+                {FILTER_TIPOCLASE_OPTIONS.map((item) => (
+                  <FormControlLabel key={item} value={item} control={<Checkbox />} label={item} />
+                ))}
+              </RadioGroup>
+            </div>
+
+            <div>
+              <Typography variant="subtitle1" gutterBottom>
+                Frecuencia
+              </Typography>
+              <RadioGroup>                {FILTER_FRECUENCIA_OPTIONS.map((item) => (
+                  <FormControlLabel key={item} value={item} control={<Checkbox />} label={item} />
+                ))}
+              </RadioGroup>
+            </div>
+
+            <div>
+              <Typography variant="subtitle1" gutterBottom>
+                Calificación
+              </Typography>
+              <RadioGroup>
+                
+                <Rating/>
+                
+              </RadioGroup>
+            </div>
+          </Stack>
+        </Scrollbar>
+
+        <Box sx={{ p: 3 }}>
+          <Button
+            fullWidth
+            size="large"
+            type="submit"
+
+            variant="contained"
+            startIcon={<Iconify icon="ic:round-clear-all" />}
+          >
+            Buscar
+          </Button>
+        </Box>
+      </Drawer>
     </>
   );
 }
